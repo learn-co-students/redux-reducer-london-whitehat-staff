6b31a0b4666b689f5aa4d4b56529beb0a559e75a
Anna Burley <anna.burley@sky.uk>
2019-05-02 14:48:38 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend);
+      return state;
+    case 'REMOVE_FRIEND':
+      const index = state.friends.findIndex(obj => obj.id === action.id);
+      const newState = [
+      ...state.friends.slice(0, index),
+      ...state.friends.slice(index + 1),
+    ];
+      return {friends: newState};
+
+    default:
+      return state;
+  }
 }
